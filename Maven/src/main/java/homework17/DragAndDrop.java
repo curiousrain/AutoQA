f7d28a9ce84e38cdc33e4e09c748f371d1f674c3
curiousrain <nazgulchan@gmail.com>
2025-05-19 01:18:43 +0500

    homework21

--- Maven/src/main/java/homework17/DragAndDrop.java
+++ Maven/src/main/java/homework17/DragAndDrop.java
@@ -7,9 +7,10 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+//import org.testng.Assert;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 
 import java.time.Duration;
 
